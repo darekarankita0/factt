2809bde006cd393420543bc6ce41ad8f816da3bc
darekarankita0 <55353280+darekarankita0@users.noreply.github.com>
2021-01-29 17:07:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,9 @@ def facts():
         value=random.choices(dff)
         #print(value)
         time.sleep(30)
-        return render_template('facts.html',value=value)
+        json_object=value.json()
+        return json_object
+        #return render_template('facts.html',value=value)
         #return " {} ".format(value)
        # return " {} ".format(value)
         #return [(" {} ".format(value)) , render_template('refresh.html')]
